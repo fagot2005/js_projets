436a923f76a3143fbaf66e5ef5b8eb2a412f71d6
fagot2005 <v.lashchenko.1976@gmail.com>
2022-12-26 14:46:59 +0200

    Add file primitive

---
+++ index.js
@@ -0,0 +1,6 @@
+//3+2
+console.log(a);
+
+let a = 'Bogdan';
+console.log(a);
+//Bogdan
---
+++ primitive.js
@@ -0,0 +1,12 @@
+const a = {
+    first: 20,
+    second: true
+}
+
+console.log(a.first);
+console.log(a.second);
+
+a.first = 50;
+
+console.log(a.first);
+console.log(a.second);
